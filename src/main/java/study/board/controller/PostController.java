667bd3862f10682a57889073510146d056a2c24f
wisekim <lwisekiml@naver.com>
2024-04-27 00:26:07 +0900

    글쓰기 기능

---
+++ src/main/java/study/board/controller/PostController.java
@@ -0,0 +1,26 @@
+package study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts/new")
+    public String createForm() {
+        return "posts/createPostForm";
+    }
+
+    @PostMapping("/posts/new")
+    public String create(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content
+    ) {
+        System.out.println("title = " + title);
+        System.out.println("content = " + content);
+
+        return "redirect:/";
+    }
+}
