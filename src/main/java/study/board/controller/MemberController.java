633eedd87aed01953a3a88ffd1bc1c38a7652979
wisekim <lwisekiml@naver.com>
2024-04-26 19:18:24 +0900

    회원 가입 및 환영 메시지

--- src/main/java/study/board/controller/MemberController.java
+++ src/main/java/study/board/controller/MemberController.java
@@ -1,13 +1,35 @@
 package study.board.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@RequestMapping("/members")
 public class MemberController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    // http://localhost:8080/members/add
+    @GetMapping("/add")
+    public String addForm() {
+        return "members/addMemberForm";
+    }
+
+    // http://localhost:8080/members/add
+    @PostMapping("/add")
+    public String save(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+        System.out.println("password = " + password);
+
+        return "redirect:/members/welcome";
+    }
+
+    // http://localhost:8080/welcome
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
