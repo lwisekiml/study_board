c92884f1eec7321d5136313ddc4c89256fb12679
wisekim <lwisekiml@naver.com>
2024-04-27 22:36:20 +0900

    Member entity 수정

--- src/main/java/study/board/entity/Member.java
+++ src/main/java/study/board/entity/Member.java
@@ -1,5 +1,6 @@
 package study.board.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,10 +15,10 @@ import lombok.Setter;
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
 
-    public Member(String username) {
-        this.username = username;
-    }
+    private String username;
+    private String email;
+    private String password;
 }
