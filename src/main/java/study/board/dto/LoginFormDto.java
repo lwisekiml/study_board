4a6f2f061631bb9e8ff04ec6de823c39dcb05dcf
wisekim <lwisekiml@naver.com>
2024-05-06 18:13:07 +0900

    로그인 쿠키 사용

--- src/main/java/study/board/dto/LoginFormDto.java
+++ src/main/java/study/board/dto/LoginFormDto.java
@@ -6,12 +6,15 @@ import lombok.Data;
 @Data
 public class LoginFormDto {
 
+    private Long id;
+
     @NotEmpty
     private String loginId;
     @NotEmpty
     private String password;
 
-    public LoginFormDto(String loginId, String password) {
+    public LoginFormDto(Long id, String loginId, String password) {
+        this.id = id;
         this.loginId = loginId;
         this.password = password;
     }
