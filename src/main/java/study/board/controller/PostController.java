a55af0955606fb4e65767fd5adf9df233b9d75c4
wisekim <lwisekiml@naver.com>
2024-05-09 10:44:11 +0900

    게시글 DB에 저장

--- src/main/java/study/board/controller/PostController.java
+++ src/main/java/study/board/controller/PostController.java
@@ -1,15 +1,21 @@
 package study.board.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.board.dto.LoginFormDto;
 import study.board.dto.PostFormDto;
+import study.board.entity.Post;
+import study.board.repository.PostRepository;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostRepository postRepository;
 
     @GetMapping("/posts/new")
     public String createForm(
@@ -24,6 +30,7 @@ public class PostController {
             HttpServletRequest request
     ) {
 
+        postRepository.save(new Post("1", form.getTitle(), form.getContent()));
 
         return "redirect:/";
     }
