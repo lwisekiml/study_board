e6b7829942a3f18c977aaab7b80058f6cc1dc43c
wisekim <lwisekiml@naver.com>
2024-05-17 21:40:09 +0900

    Comment 관련 틀(?)

---
+++ src/main/java/study/board/comment/Comment.java
@@ -0,0 +1,23 @@
+package study.board.comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+}
