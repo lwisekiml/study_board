f613e604a41324942b4c9f7d60e5abe822c30480
wisekim <lwisekiml@naver.com>
2024-05-23 17:53:40 +0900

    등록일, 수정일 추가
    - 등록인, 수정인은 후에 로그인 기능이 있을 때 수정 필요

--- src/main/java/study/board/BoardApplication.java
+++ src/main/java/study/board/BoardApplication.java
@@ -2,7 +2,14 @@ package study.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 
@@ -10,4 +17,15 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		// 스프링 시큐리티에서 세션 정보를 가져와서 아이디를 꺼내어 사용하면 된다.
+		return () -> Optional.of(UUID.randomUUID().toString());
+//		return new AuditorAware<String>() {
+//			@Override
+//			public Optional<String> getCurrentAuditor() {
+//				return Optional.of(UUID.randomUUID().toString());
+//			}
+//		};
+	}
 }
