e8b00015dad8998a0f0eac7792486cac0319817b
wisekim <lwisekiml@naver.com>
2024-05-24 22:26:27 +0900

    영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환

--- src/test/java/study/board/testenti/MemberTest.java
+++ src/test/java/study/board/testenti/MemberTest.java
@@ -1,15 +1,11 @@
 package study.board.testenti;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -91,5 +87,62 @@ class MemberTest {
 
         System.out.println("findMember.getUsername() = " + findMember.getUsername()); // 쿼리 x
         System.out.println("findMember.getClass() = " + findMember.getClass());
+    }
+
+    @Test
+    public void 영속성_컨텍스트에_찾는_엔티티가_이미_있으면_getReference_를_호출해도_실제_엔티티_반환() {
+        Member member1 = new Member();
+        member1.setUsername("member1");
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("member2");
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        // 1. find 와 find 비교
+//        Member m1 = em.find(Member.class, member1.getId()); // Member
+//        Member m2 = em.find(Member.class, member2.getId()); // Member
+//
+//        System.out.println("m1 = " + m1.getClass()); // m1 = class hellojpa.Member
+//        System.out.println("m2 = " + m2.getClass()); // m2 = class hellojpa.Member
+//        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
+
+        // 2. find 와 getReference 비교
+//        Member m3 = em.find(Member.class, member1.getId()); // Member
+//        Member m4 = em.getReference(Member.class, member2.getId()); // Proxy
+//
+//        System.out.println("m3 = " + m3.getClass()); // Member
+//        System.out.println("m4 = " + m4.getClass()); // Proxy
+//        System.out.println("m3 == m4 : " + (m3.getClass() == m4.getClass())); // false
+//        System.out.println("m3 : " + (m3 instanceof Member)); // true
+//        System.out.println("m3 : " + (m4 instanceof Member)); // true
+
+        // 3. 영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환
+        // find -> getReference : Member
+//        Member m1 = em.find(Member.class, member1.getId()); // Member
+//        Member m2 = em.getReference(Member.class, member1.getId()); // Member
+//        System.out.println("m1 = " + m1.getClass()); // Member
+//        System.out.println("m2 = " + m2.getClass()); // Member
+//        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
+
+        // getReference -> find : Proxy
+//        Member m1 = em.getReference(Member.class, member1.getId()); // Proxy
+//        Member m2 = em.find(Member.class, member1.getId()); // Proxy
+//        System.out.println("m1 = " + m1.getClass()); // Proxy
+//        System.out.println("m2 = " + m2.getClass()); // Proxy
+//        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
+
+        // 4. detach
+//        Member refMember = em.getReference(Member.class, member1.getId());
+//        System.out.println("reference.getClass() = " + refMember.getClass());
+//
+//        em.detach(refMember);
+//        em.clear();
+//        em.close();
+//
+//        System.out.println("refMember.getUsername() = " + refMember.getUsername()); // LazyInitializationException
     }
 }
