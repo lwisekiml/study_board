3004914f986db5f6dbe096f7720f3a63711e8ad8
wisekim <lwisekiml@naver.com>
2024-05-24 21:14:12 +0900

    Member 디렉토리꺼 주석 처리

--- src/main/java/study/board/member/MemberFormDto.java
+++ src/main/java/study/board/member/MemberFormDto.java
@@ -1,18 +1,18 @@
-package study.board.member;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-@AllArgsConstructor
-public class MemberFormDto {
-
-    private String username;
-    private String loginId;
-    private String password;
-
-    public MemberFormDto() {
-    }
-
-}
+//package study.board.member;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter @Setter
+//@AllArgsConstructor
+//public class MemberFormDto {
+//
+//    private String username;
+//    private String loginId;
+//    private String password;
+//
+//    public MemberFormDto() {
+//    }
+//
+//}
--- src/main/java/study/board/member/MemberRepository.java
+++ src/main/java/study/board/member/MemberRepository.java
@@ -1,8 +1,8 @@
-package study.board.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import study.board.member.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByLoginIdAndPassword(String loginId, String password);
-}
+//package study.board.member;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import study.board.member.Member;
+//
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//    Member findByLoginIdAndPassword(String loginId, String password);
+//}
