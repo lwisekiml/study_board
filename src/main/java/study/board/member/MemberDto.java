9fda11f2877a1cc85e19ba4ce0a8238ae5193a61
wisekim <lwisekiml@naver.com>
2024-06-02 18:17:37 +0900

    CommentDto에 불필요한 Board, Member를 boardId, loginId로 수정

--- src/main/java/study/board/member/MemberDto.java
+++ src/main/java/study/board/member/MemberDto.java
@@ -33,4 +33,8 @@ public class MemberDto {
 //    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다."
 //    private String phoneNumber;
 //    private String address;
+
+    public static String toLoginId(Member entity) {
+        return entity.getLoginId();
+    }
 }
