865a1ba1124e397e0c5e62e595b28ad9feaaec2f
wisekim <lwisekiml@naver.com>
2024-06-03 10:41:15 +0900

    member 관련 수정(영향 없는 수정) - log 수정 - 주석 수정

--- src/main/java/study/board/member/MemberController.java
+++ src/main/java/study/board/member/MemberController.java
@@ -39,7 +39,6 @@ public class MemberController {
 
         // 비밀번호와 비밀번호 확인 값이 같은지 검증
         if (!memberDto.getPassword().equals(memberDto.getPasswordConfirm())) {
-            // bindingResult.rejectValue(필드명, 오류 코드, 오류 메시지)
             bindingResult.rejectValue("passwordConfirm", "passwordInCorrect", "비밀번호와 비밀번호 확인의 값이 일치하지 않습니다.");
             return "/member/signup";
         }
@@ -47,12 +46,12 @@ public class MemberController {
         try {
             memberService.create(memberDto);
         } catch (DataIntegrityViolationException e) {
-            e.printStackTrace();
+            log.error("signupFailed!! Duplicate!! " + memberDto, e);
             // 아이디 중복인지 이메일 중복인지 분리하려 했으나 방법을 못찾았다.
             bindingResult.reject("signupFailed", "이미 등록된 사용자입니다."); // 글로벌 오류
             return "/member/signup";
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("signupFailed!! " + memberDto, e);
             bindingResult.reject("signupFailed", e.getMessage());
             return "/member/signup";
         }
