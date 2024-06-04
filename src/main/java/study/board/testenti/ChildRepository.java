e6c216fa82c9182cd8e266071ecf88b961268a58
wisekim <lwisekiml@naver.com>
2024-06-04 22:24:15 +0900

    공부용 파일 삭제

--- src/main/java/study/board/testenti/ChildRepository.java
+++
@@ -1,6 +0,0 @@
-package study.board.testenti;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChildRepository extends JpaRepository<Child, Long> {
-}
--- src/main/java/study/board/testenti/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package study.board.testenti;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
