08d9783cfb52715b2f604fa5a478fdafcb1d4861
wisekim <lwisekiml@naver.com>
2024-06-10 15:03:10 +0900

    @Lazy 사용하여 순화참조 방지되나 근본적인 해결책은 아니므로 수정 필요
    The dependencies of some of the beans in the application context form a cycle:
    
    ┌─────┐
    |  securityConfig defined in file [G:\study_board\out\production\classes\study\board\config\SecurityConfig.class]
    ↑     ↓
    |  customOAuth2UserService defined in file [G:\study_board\out\production\classes\study\board\oauth2\CustomOAuth2UserService.class]
    └─────┘
    
    Action:
    
    Relying upon circular references is discouraged and they are prohibited by default. Update your application to remove the dependency cycle between beans. As a last resort, it may be possible to break the cycle automatically by setting spring.main.allow-circular-references to true.

--- src/main/java/study/board/config/SecurityConfig.java
+++ src/main/java/study/board/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package study.board.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
