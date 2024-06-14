f8fdb1ea0dc2e1f76ab06745aa76f5a266956e54
wisekim <lwisekiml@naver.com>
2024-06-14 14:45:11 +0900

    aws에서 SocialClientRegistration 문제로 application.yml 설정으로 수정

--- src/main/java/study/board/oauth2/CustomClientRegistrationRepo.java
+++ src/main/java/study/board/oauth2/CustomClientRegistrationRepo.java
@@ -1,20 +1,20 @@
-package study.board.oauth2;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-
-@Configuration
-@RequiredArgsConstructor
-public class CustomClientRegistrationRepo {
-
-    private final SocialClientRegistration socialClientRegistration;
-
-    public ClientRegistrationRepository clientRegistrationRepository() {
-
-        return new InMemoryClientRegistrationRepository(
-                socialClientRegistration.kakaoClientRegistration()
-        );
-    }
-}
+//package study.board.oauth2;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+//import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class CustomClientRegistrationRepo {
+//
+//    private final SocialClientRegistration socialClientRegistration;
+//
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//
+//        return new InMemoryClientRegistrationRepository(
+//                socialClientRegistration.kakaoClientRegistration()
+//        );
+//    }
+//}
