ebccff5481f61d269147c018d9104a6e75de5a59
wisekim <lwisekiml@naver.com>
2024-07-10 21:42:50 +0900

    이메일 인증 코드 전송 테스트 - EmailConfig 사용

--- src/main/java/study/board/mail/EmailController.java
+++ src/main/java/study/board/mail/EmailController.java
@@ -1,24 +1,25 @@
 package study.board.mail;
 
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.UnsupportedEncodingException;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class EmailController {
 
-    private final EmailService emailService;
+    private final EmailService2 emailService2;
 
-    @ResponseBody
-    @PostMapping("/sign-up/emailCheck")
-    public String emailCheck(@RequestBody EmailCheckReq emailCheckReq) throws MessagingException, UnsupportedEncodingException {
-        emailService.sendEmail(emailCheckReq.getEmail());
-        return "ok";
+    @PostMapping("/emailConfirm")
+    public String emailConfirm(@RequestParam("email") String email) throws Exception {
+        return emailService2.sendSimpleMessage(email);
     }
+
+//    @ResponseBody
+//    @PostMapping("/sign-up/emailCheck")
+//    public String emailCheck(@RequestBody EmailCheckReq emailCheckReq) throws MessagingException, UnsupportedEncodingException {
+//        emailService.sendEmail(emailCheckReq.getEmail());
+//        return "ok";
+//    }
 }
---
+++ src/main/java/study/board/mail/EmailService2.java
@@ -0,0 +1,5 @@
+package study.board.mail;
+
+public interface EmailService2 {
+    String sendSimpleMessage(String to)throws Exception;
+}
