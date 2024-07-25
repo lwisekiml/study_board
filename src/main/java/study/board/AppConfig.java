073b773d0eef11b2572e311db60ea47951cae1f9
wisekim <lwisekiml@naver.com>
2024-07-25 17:00:22 +0900

    AppConfig로 docker 사용 설정가능하도록 수정

--- src/main/java/study/board/AppConfig.java
+++ src/main/java/study/board/AppConfig.java
@@ -1,19 +1,28 @@
 package study.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import study.board.board.Board;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import study.board.config.RedisConfig;
 import study.board.mail.EmailAuthentication;
 import study.board.mail.VerificationCodeRepository;
 import study.board.util.RedisUtil;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final RedisConfig redisConfig;
 
     @Bean
     public EmailAuthentication emailAuthentication() {
-        return new VerificationCodeRepository();
+//        return new VerificationCodeRepository();
         // docker 사용할 때
-//        return new RedisUtil();
+        return new RedisUtil(stringRedisTemplate());
+    }
+
+    public StringRedisTemplate stringRedisTemplate() {
+        return new StringRedisTemplate(redisConfig.redisConnectionFactory());
     }
 }
